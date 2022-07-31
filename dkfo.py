363d5a671c627bb64f5f6a8a0d4557330da16836
Denis Kislitsyn <sournk@gmail.com>
2022-07-31 12:40:57 +0300

    First commit

---
+++ dkfo.py
@@ -0,0 +1,29 @@
+# dkfo - File Orginizer Script
+# Author: Denis Kislitsyn
+# Email: denis@kislitsyn.me
+#   
+# Script gets one param - src dir path.
+# Script moves all files from src dir into !Archive subdir.
+# Script is useful for Downloads folder. Just add script into your cron task.
+
+import os
+import sys
+import time
+import shutil
+
+# Subdir name is destanation for moving files in
+ARCHIVE_DIR_NAME = '!Archive'
+
+path_src = sys.argv[1]
+path_dst = os.path.join(path_src, ARCHIVE_DIR_NAME)
+
+files = sorted(filter(os.path.isfile, [os.path.join(path_src, f) for f in os.listdir(path_src)]))
+for f in files:
+    day = time.strftime('%d', time.gmtime(os.path.getctime(f)))
+    path_dst_day = os.path.join(path_dst, day)
+    os.makedirs(path_dst_day, exist_ok=True)
+    try:
+        shutil.move(f, os.path.join(path_dst_day, os.path.basename(f)))
+        print(f'[INFO] File archived {f}')
+    except Exception as e:
+        print(f'[ERROR] File is not moved: {str(e)}')
