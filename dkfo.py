27589ab10dae02316e6e42816dc83d9ef9353099
Denis Kislitsyn <sournk@gmail.com>
2022-07-31 16:02:18 +0300

    + You can skip moving files older than 2 days from src_dir * Now checks mdate instead cdate

--- dkfo.py
+++ dkfo.py
@@ -2,22 +2,36 @@
 # Author: Denis Kislitsyn
 # Email: denis@kislitsyn.me
 #   
-# Script gets one param - src dir path.
-# Script moves all files and subdirs from src dir into !Archive/%dd subdirs.
+# Script gets one param - src_dir path.
+# Script moves all files and subdirs older than 2 days from src_dir into !Archive/%dd subdirs.
 # Script is useful for Downloads folder. Just add script into your cron task.
 
 import os
 import sys
 import time
 import shutil
+import datetime as dt
 
-# Subdir name is destanation for moving files in
-ARCHIVE_DIR_NAME = '!Archive'
+ARCHIVE_DIR_NAME = '!Archive' # Subdir name is destanation for moving files in
+DONT_TOUCH_FILES_DEPTH_DAYS = 2 # Paths with cdate early today()-DONT_TOUCH_FILES_DEPTH_DAYS are skiped
 
+def is_old_file(file):
+    '''
+    Check creation date of file. If it's created earlier then DONT_TOUCH_FILES_DEPTH_DAYS days file is old.
+    '''
+    
+    old_file_edge = dt.datetime.today() - dt.timedelta(days=DONT_TOUCH_FILES_DEPTH_DAYS)
+    old_file_edge = old_file_edge.timestamp()
+    return os.path.getmtime(file) < old_file_edge
+    
 path_src = sys.argv[1]
 path_dst = os.path.join(path_src, ARCHIVE_DIR_NAME)
 
-files = sorted(filter(lambda path: path != path_dst, [os.path.join(path_src, f) for f in os.listdir(path_src)]))
+files = [os.path.join(path_src, f) for f in os.listdir(path_src)] # list of full paths files and dirs
+files = filter(lambda path: path != path_dst, files) # delete archive dir from list, becasue it's not necessary to move this dir from top level
+files = filter(is_old_file, files) # filter earlier paths from list 
+files = sorted(files)
+
 for f in files:
     day = time.strftime('%d', time.gmtime(os.path.getctime(f)))
     path_dst_day = os.path.join(path_dst, day)
